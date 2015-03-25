903572b828462a93be995b60af51630bee2b9700
Kamil Kubica <info@kamilkubica.com>
2015-03-25 15:19:21 +0100

    demo commit

--- cherry-picker.js
+++ cherry-picker.js
@@ -3,52 +3,65 @@
 var exec = require('child_process').exec
   , readline = require('readline')
   , Q = require('q')
-  , deferred = Q.defer()
   , tickets = [];
 
 Q.fcall(hello)
   .then(getTicket)
-  .then(getHases);
+  .then(getHashes)
+  .then(printHashes);
 
 function hello() {
   console.log("###############\n");
   console.log("let's pick the cherrys");
   console.log("type in the full ticket name like 'LHAS-1337'");
   console.log("this tool will output the SHA commit hases for give tickets");
-  console.log("\n###############");
+  console.log("\n###############\n");
 }
 
 function getTicket() {
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  console.log("getTicket()");
+  var deferred = Q.defer();
 
-  rl.question("Ticket number pls or 'done': ", function(ticketString) {
-    rl.close();
+  var read = function() {
+    console.log("read()");
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-    if (ticketString === "done") {
-      deferred.resolve(tickets);
-    } else {
-      if (ticketString.length) {
-        tickets.push(ticketString);
+    rl.question("Ticket number pls or 'done': ", function(ticketString) {
+      rl.close();
+
+      if (ticketString === "done") {
+        deferred.resolve(tickets);
+      } else {
+        if (ticketString.length) {
+          tickets.push(ticketString);
+        }
+
+        console.log("your tickets", tickets);
+        read();
       }
+    });
+  };
 
-      console.log("your tickets", tickets);
-      getTicket();
-    }
-  });
+  read();
 
   return deferred.promise;
 }
 
-function getHases(tickets) {
+function getHashes(tickets) {
+  console.log("getHashes()");
   var promises = [];
 
   tickets.forEach(function(ticket) {
-    getLog(ticket, function(result) {
+    var deferred = Q.defer();
+
+    getLog(ticket).then(function(result) {
+      console.log("getLog done", result);
       deferred.resolve(result);
     });
+
     promises.push(deferred.promise);
   });
 
@@ -56,19 +69,28 @@ function getHases(tickets) {
 }
 
 function getLog(ticket) {
-  console.log("getLog");
-  var cmd = "git log --grep='" + ticket + "' | grep 'commit' | sed 's/commit/git cherry-pick/g'";
+  console.log("getLog()");
+  var deferred = Q.defer();
+  var cmd = "git log --grep=\"" + ticket + "\" | grep \"commit\" | sed \"s/commit/git cherry-pick/g\"";
 
-  console.log("cmd: ", cmd);
   exec(cmd, function(error, stdout, stderr) {
     if (error) {
+      console.log(stderr);
       deferred.reject(error, stderr);
     } else {
-      console.log("out");
-      console.log(stdout);
+      console.log("stdout", stdout);
       deferred.resolve(stdout);
     }
   });
 
   return deferred.promise;
+}
+
+function printHashes(logs) {
+  console.log("printHashes()");
+
+  logs.forEach(function(log, qwe) {
+    console.log(log, qwe);
+    // console.log(log.replace("\n", ""));
+  });
 }
